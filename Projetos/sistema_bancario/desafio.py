f57d61036413ab11f4e034925ca98726f242de49
gabrielcleonardo <gabrielcleonardo@hotmail.com>
2025-05-03 14:23:40 -0300

    withdraw

--- Projetos/sistema_bancario/desafio.py
+++ Projetos/sistema_bancario/desafio.py
@@ -8,7 +8,7 @@ menu = """
 =>"""
 balance = 0
 statament = ""
-limit = 500
+limit_value = 500
 withdraws_limit = 3
 withdraws_count = 0
 
@@ -16,7 +16,7 @@ while True:
 
     user_option = int(input(menu))
 
-    if user_option == 1:
+    if user_option == 1: ##DEPOSIT##
         try:
             deposit = float(input("Amount do you want to deposit: "))
             if deposit > 0:
@@ -29,11 +29,35 @@ while True:
         except ValueError:
             print("==== Invalid Value, try again ====")
 
+    elif user_option == 2: ## WITHDRAWN ##
+
+        if withdraws_count >= withdraws_limit:
+            print("Number of withdraws exceeded")
+
+        else:
+            value = float(input("Amount do you want to withdraw: "))
+
+            if value > limit_value:
+                print(f"Your limit is {limit_value}, please try again")
+
+            elif value > balance:
+                print(f"Your balance is {balance}, please try again")
+
+            elif value <= 0:
+                print("Invalid value")
+
+            else:
+                balance -= value
+                withdraws_count += 1
+                statament += (f"R$ {value:.2f}\n")
+                print(f"Withdrawal of R$ {value:.2f} successfully completed")
+
+
     elif user_option == 4:
         break
 
     else:
-        print("Try again")
+        print("==== Please, Try again ====")
 
 
 
