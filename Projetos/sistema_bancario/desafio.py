c6fa23559cddd94bfacdf100e63bdbd1e5dc98b7
gabrielcleonardo <gabrielcleonardo@hotmail.com>
2025-05-05 11:45:22 -0300

    extrato e textos finais

--- Projetos/sistema_bancario/desafio.py
+++ Projetos/sistema_bancario/desafio.py
@@ -1,63 +1,67 @@
 menu = """
 ==== Enter the desired option =====
 [1] Deposit
-[2] Withdraw
+[2] Withdrawl
 [3] Statement
 [4] Exit
 
 =>"""
 balance = 0
-statament = ""
+statement = ""
 limit_value = 500
-withdraws_limit = 3
-withdraws_count = 0
+withdrawl_limit = 3
+withdrawl_count = 0
 
 while True:
 
-    user_option = int(input(menu))
+    user_option = int(input(menu).strip())
 
     if user_option == 1: ##DEPOSIT##
-        try:
-            deposit = float(input("Amount do you want to deposit: "))
-            if deposit > 0:
-                balance += deposit
-                statament += f"R$ {deposit:.2f}\n"
-                print(f"R$ {deposit:.2f} deposited successfully")
 
-            else:
-                print("==== No value to deposit ====")
-        except ValueError:
-            print("==== Invalid Value, try again ====")
-
-    elif user_option == 2: ## WITHDRAWN ##
-
-        if withdraws_count >= withdraws_limit:
-            print("Number of withdraws exceeded")
+        deposit = float(input("Enter the amount you want to deposit: ").strip())
+        if deposit > 0:
+            balance += deposit
+            statement += f"Deposit: +R$ {deposit:.2f}\n"
+            print(f"R$ {deposit:.2f} deposited successfully")
 
         else:
-            value = float(input("Amount do you want to withdraw: "))
+            print("==== Invalid amount. Deposit must be greater than zero. ====")
+
+    elif user_option == 2: ## WITHDRAWL ##
+
+        if withdrawl_count >= withdrawl_limit:
+            print("==== Daily number of withdrawals exceeded. ====")
+
+        else:
+            value = float(input("Enter the amount you want to withdraw: ").strip())
 
             if value > limit_value:
-                print(f"Your limit is {limit_value}, please try again")
+                print(f"==== Your limit is {limit_value}, please try again ====")
 
             elif value > balance:
-                print(f"Your balance is {balance}, please try again")
+                print(f"==== Insufficient balance. Your balance is ${balance:.2f}. ====")
 
             elif value <= 0:
-                print("Invalid value")
+                print("==== Invalid amount. ====")
 
             else:
                 balance -= value
-                withdraws_count += 1
-                statament += (f"R$ {value:.2f}\n")
-                print(f"Withdrawal of R$ {value:.2f} successfully completed")
+                withdrawl_count += 1
+                statement += (f"Withdrawl: -R$ {value:.2f}\n")
+                print(f"Withdrawal of R$ {value:.2f} completed successfully")
 
-
+    elif user_option == 3: ## STATEMENT
+        print("\n======== Bank Statement ========")
+        print("No transactions found") if not statement else print(statement)
+        print("======== Current Balance ========")
+        print(f"R$ {balance:.2f}")
+        print("================================\n")
     elif user_option == 4:
+        print("==== Thank you for using our banking system. Goodbye! ====")
         break
 
     else:
-        print("==== Please, Try again ====")
+        print("==== Invalid option. Please try again. ====")
 
 
 
